52d6e58515ba12277288f49cde9bda70416d37bb
Nikolaj Korobov <nickkorobov3d@gmail.com>
2017-12-18 11:37:17 +0300

    another assert for checking add work

--- task.py
+++ task.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 def add(a, b):
-    # TODO: реализуйте сложение двух чисел
-    pass
+    # CHANGING FILES
+    return a + b
 
 def test_add():
     assert add(2, 2) == 4
     assert add(5, 10) == 15
-
+    assert add(2, 5) == 7
 def main():
     print("Hello World")
 
